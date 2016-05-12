7ed9cb6c0c45b4cc354c3d79227d7247c35cea22
lagka <lagkanacl@gmail.com>
2016-05-12 21:35:53 +0800

    no message

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,78 +1,98 @@
 package main.java;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.HashMap;
-import processing.core.PApplet;
-
-/**
-* This class is used to store states of the characters in the program.
-* You will need to declare other variables depending on your implementation.
-*/
 public class Character {
-	
+
 	private MainApplet parent;
-	private String name;
-	private int color, weight, radius;
-	private float cur_x, cur_y, init_x, init_y;
-	//private ArrayList<Character> targets;
+	public String name;
+	float cur_x, cur_y;
+	float init_x, init_y; // Initial position of X and Y
+	private int color;
+	public int diameter = 40;
+	boolean inBigCircle = false, hover = false;
+	
+	//private ArrayList<Character> targets; // store the target
+	//private ArrayList<Character> sources; // store the source
+	public int weight = 0; // the weight of line
 	private HashMap<Character, Integer> relations;
-	private boolean in_circle;
 
-	public Character(MainApplet parent, String name, String color, float x, float y){
-
+	@SuppressWarnings("static-access")
+	public Character(MainApplet parent, String name, String color, int x, int y) {
 		this.parent = parent;
 		this.name = name;
-		this.color = (int) Long.parseLong(color.replace("#", ""), 16);
-		this.init_x = x;
-		this.init_y = y;
-		this.cur_x = this.init_x;
-		this.cur_y = this.init_y;
-		this.radius = 40;
-		//this.targets = new ArrayList<Character>();
+		this.cur_x = x + 30;
+		this.cur_y = y + 30;
+		this.init_x = this.cur_x;
+		this.init_y = this.cur_y;
+		this.color = this.parent.unhex(color.substring(1));
+		//targets = new ArrayList<Character>();
+		//sources = new ArrayList<Character>();
 		this.relations = new HashMap<Character, Integer>();
 	}
 
-	public void display(){
-		this.parent.fill(this.color);
-		this.parent.ellipse(this.cur_x, this.cur_y, this.radius, this.radius);
-		
-		if (in_circle) {
+	public void display() {
+		parent.noStroke();
+		parent.fill(color);
+		parent.ellipse(this.cur_x, this.cur_y, diameter, diameter);
+		// if inBigCircle equals to true, do the network
+		if (inBigCircle) {
+			// For the draw two times.
+			// I want to check the node have something to do with which nodes.
+			// This is the reason why I draw the line from target to source
+			// and from source to target.
 			for (Character c : relations.keySet()) {
-				this.parent.noFill();
-				this.parent.stroke(0);
-				this.parent.strokeWeight(this.relations.get(c));
+				parent.noFill();
+				parent.stroke(0);
+				if(hover)
+					parent.strokeWeight(this.relations.get(c));
+				else
+					parent.strokeWeight(weight);
+					
 				float a = (600 + (cur_x + c.cur_x) / 2) / 2;
 				float b = (355 + (cur_y + c.cur_y) / 2) / 2;
-				if (c.in_circle)
+				if (c.inBigCircle)
 					parent.bezier(cur_x, cur_y, a, b, a, b, c.cur_x, c.cur_y);
 			}
 		}
 	}
-	
-	public void return_orign_palce()
-	{
-		this.cur_x = this.init_x;
-		this.cur_y = this.init_y;
-	}
-	
-	public void add_target(Character ch, int weight) {
-		//targets.add(ch);
-		//weights.add(weight);
-		this.relations.put(ch, weight);
-	}
-	
-	public float get_init_x()
-	{
-		return this.init_x;
-	}
-	
-	public float get_init_y()
-	{
-		return this.init_x;
-	}
-	
-	public String get_name()
-	{
+
+	// return the node's name
+	public String getName() {
 		return this.name;
+	}
+
+	// return the initial position of X
+	public float getInitX() {
+		return init_x;
+	}
+
+	// return the initial position of Y
+	public float getInitY() {
+		return init_y;
+	}
+	
+	public HashMap<Character, Integer> get_relations()
+	{
+		return this.relations;
+	}
+
+	// set the diameter of the node
+	public void setDiameter(int diameter) {
+		this.diameter = diameter;
+	}
+
+	// set the weight of line
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public void set_hover(boolean b) {
+		this.hover = b;
+	}
+
+	// add the target of JSONObject to the list(targets)
+	public void addTarget(Character ch, int value) {
+		this.relations.put(ch, value);
 	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,10 +1,8 @@
 package main.java;
 
-
-
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-
+import controlP5.ControlFont;
 import controlP5.ControlP5;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
@@ -13,78 +11,99 @@ import processing.core.PApplet;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
 
-/**
-* This class is for sketching outcome using Processing
-* You can do major UI control and some visualization in this class.  
-*/
 @SuppressWarnings("serial")
-public class MainApplet extends PApplet{
-	private String path = "main/resources/";
-	private String file = "starwars-episode-";
-	private String file2 ="-interactions.json";
-	private int level =1;
-	private JSONObject datas;
-	private JSONArray nodes;
-	private JSONArray links;
-	private final static int width = 1000, height = 670;
-	private ArrayList<Character> characters=new  ArrayList<Character>();
-	private boolean mt;
-	private  ControlP5 button;
-	private Minim minim;
-	private AudioPlayer media;
-	private Ani animation;
-	private int circleX,circleY;
-	public void setup() {
-
-		size(width, height);
-		smooth();
-		loadData();
-		//minim=new Minim(this);
-		//media=minim.loadFile(this.getClass().getResource("main/resources/VOIA.mp3").getPath());
-		//media.play();
-		Ani.init(this);
-		button=new ControlP5(this);
+public class MainApplet extends PApplet {
 	
-		button.addButton("ButtonAdd").setLabel("ADD ALL Nodes")
-									 .setPosition(600, 40) 	
-									 .setSize(150, 40);
-		button.addButton("ButtonClear").setLabel("Clear")
-		 							   .setPosition(600, 80) 	
-		 							   .setSize(150, 40);
-		
+	private final static int width = 1200, height = 650;
+	private String path = "main/resources/";
+	private int level = 1;
+	private String file;
+	private JSONObject data;
+	private JSONArray nodes, links;
+	private ArrayList<Character> characters;
+	
+	private ControlP5 cp5;
+	@SuppressWarnings("unused")
+	private Ani ani;
+	private Minim minim;
+	private AudioPlayer bgMusic;
+	private int m=0, n=0;
+	private Character locked;// the Character object which is being to drag
+	private Character isOverAndNotPress;// mouse is over the Character object but not press it
+	private boolean isOver;// check if the mouse is over the Character object
+	private ArrayList<Character> addOrder;// the order add to the BigCircle
+	// check if List(addOrder) have the same one while mouseReleased in the big circle
+	private boolean haveSame = false;
+	
+	public void setup() {
+		size(width, height);
+		Ani.init(this);
+		minim = new Minim(this);
+		//load a music to set the background music
+		bgMusic = minim.loadFile("/main/resources/VOIA.mp3");
+		bgMusic.play();
+		cp5 = new ControlP5(this);
+		//create button
+		cp5.addButton("buttonA").setLabel("ADD ALL").setPosition(950,50).setSize(200,50);
+		cp5.addButton("buttonB").setLabel("CLEAR").setPosition(950,150).setSize(200,50);
+		// set the Font of word in the button
+		ControlFont font = new ControlFont(createFont("Consolas",20,true),241);
+		cp5.getController("buttonA").getCaptionLabel().setFont(font).setSize(24);
+		cp5.getController("buttonB").getCaptionLabel().setFont(font).setSize(24);
+		smooth();	
+		loadData();
 	}
 
 	public void draw() {
-			this.background(255);
-			this.fill(0);
-			this.textSize(40);
-			this.text("Star War "+this.level,300,40);
-			for(Character c:characters){
-				c.display();
+		background(255);
+		//set the Big Circle
+		noFill();
+		stroke(25,115,25);
+		if (locked != null && dist(locked.cur_x, locked.cur_y, width/2, height/2+30) < 250) strokeWeight(10);
+		else strokeWeight(5);
+		ellipse(width/2, height/2+30, 500, 500);
+		//set the text
+		fill(111,94,54);
+		textSize(34);
+		text("Star Wars " + level, 500, 50);
+		// if mouse is above the one of nodes and not pressed, set the new diameter of this node
+		// and let the isOverAndNotPress point to the location same as this node
+		for(Character i : characters) {
+			if(dist(i.cur_x, i.cur_y, mouseX, mouseY) < 20 && !mousePressed) {
+				isOverAndNotPress = i;
+				isOverAndNotPress.setDiameter(50);
+				isOverAndNotPress.setWeight(3);
+				i.set_hover(true);
+				System.out.println(i.get_relations().get(i));
 			}
-	}
-	public void ButtonAdd(){
-		for(Character c:characters){
-			//c.setInCircle(true);
+			else {
+				i.set_hover(false);
+				i.setDiameter(40);
+				i.setWeight(0);
+			}
+			i.display();
 		}
-			AddToCircle();
-	}
-	public void ButtonClear(){
-		
-		for(Character c:characters){
-			//c.setInCircle(false);
-			//animation = Ani.to(c, (float) 0.2, "x", c.get_init_x(),Ani.LINEAR);
-			//animation = Ani.to(c, (float) 0.2, "y", c.get_init_y(),Ani.LINEAR);
+		// check whether the mouse is above one of nodes or not
+		for (Character i : characters) {
+			if (dist(i.cur_x, i.cur_y, mouseX, mouseY) < 20) {
+				isOver = true;
+				break;
+			} 
+			else isOver = false;
 		}
-		
+		// if mouse is above one of nodes, show the name of this node
+		if (isOverAndNotPress != null) {
+			if (dist(isOverAndNotPress.cur_x, isOverAndNotPress.cur_y, mouseX, mouseY) < 20) {
+				fill(74, 224, 89);
+				rect(mouseX+10, mouseY-15, isOverAndNotPress.getName().length()*15, 30, 100);
+				fill(255);
+				textSize(16);
+				text(isOverAndNotPress.getName(), mouseX+20, mouseY+5);
+			}
+		}
 	}
-	public void mouseReleased(){
-		
-	}
-	public void mouseDragged(){
-		
-	}
-	public void keyPressed(){
+	
+	public void keyPressed(KeyEvent ke) {
 		if(keyCode==KeyEvent.VK_1||(keyCode==KeyEvent.VK_UP&&level==2)||(keyCode==KeyEvent.VK_DOWN&&level==7))level=1;
 		else if(keyCode==KeyEvent.VK_2||(keyCode==KeyEvent.VK_DOWN&&level==1)||(keyCode==KeyEvent.VK_UP&&level==3))level=2;
 		else if(keyCode==KeyEvent.VK_3||(keyCode==KeyEvent.VK_DOWN&&level==2)||(keyCode==KeyEvent.VK_UP&&level==4))level=3;
@@ -92,38 +111,105 @@ public class MainApplet extends PApplet{
 		else if(keyCode==KeyEvent.VK_5||(keyCode==KeyEvent.VK_DOWN&&level==4)||(keyCode==KeyEvent.VK_UP&&level==6))level=5;
 		else if(keyCode==KeyEvent.VK_6||(keyCode==KeyEvent.VK_DOWN&&level==5)||(keyCode==KeyEvent.VK_UP&&level==7))level=6;
 		else if(keyCode==KeyEvent.VK_7||(keyCode==KeyEvent.VK_DOWN&&level==6)||(keyCode==KeyEvent.VK_UP&&level==1))level=7;
-	
+		loadData();
 	}
-	
-
+	// load the data what we want to use from JSON file 
 	private void loadData(){
-		for(int i=1;i<=7;i++){
-			file = new String("starwars-episode-" +i+ "-interactions.json");
-			datas = loadJSONObject(path + file);
-			nodes = datas.getJSONArray("nodes");
-			links = datas.getJSONArray("links");
+		addOrder = new ArrayList<Character>();
+		characters = new ArrayList<Character>();
+		file = "starwars-episode-" + level + "-interactions.json";
+		data = loadJSONObject(path + file);
+		nodes = data.getJSONArray("nodes");
+		links = data.getJSONArray("links");
+		
+		for(int i=0; i<nodes.size(); i++) {
+			JSONObject node = nodes.getJSONObject(i);
+			String name = node.getString("name");
+			String color = node.getString("colour");
+			m = i/10;
+			n = (i>9) ? i-i/10*10 : i;
+			Character character = new Character(this,name,color,m*60,10+n*60);
+			characters.add(character);
 		}
-		circleX=50;
-		circleY=50;
-		int i;
-		for(i=0;i<nodes.size();i++){
-			JSONObject node=nodes.getJSONObject(i);
-			String name=node.getString("name");
-			String color=node.getString("colour");
-			if (characters.size() % 10 != 9) {
-				characters.add(new Character(this, name, color, circleX, circleY));
-				circleY += 60;
-			} else {
-				characters.add(new Character(this, name, color, circleX, circleY));
-				circleX += 60;
-				circleY = 50;
+		
+		for(int i=0; i<links.size(); i++) {
+			JSONObject link = links.getJSONObject(i);
+			int source = link.getInt("source");
+			int target = link.getInt("target");
+			int value = link.getInt("value");
+			characters.get(source).addTarget(characters.get(target), value);
+			characters.get(target).addTarget(characters.get(source), value);
+		}
+	}
+	// if press the buttonA(ADD ALL) add each node to the big circle
+	public void buttonA() {
+		for(Character i : characters) {
+			if(!i.inBigCircle) {
+				i.inBigCircle = true;
+				addOrder.add(i);
 			}
 		}
-		
+		addToBigCircle();
 	}
-	private void AddToCircle(){
-		
-		
+	// if press the buttonB(CLEAR) remove each node from the big circle
+	public void buttonB() {
+		for(Character i : characters) {
+			ani = Ani.to(i, (float) 1, "x", i.getInitX());
+			ani = Ani.to(i, (float) 1, "y", i.getInitY());
+			i.inBigCircle = false;
+		}
+		addOrder.clear();
+	}
+	
+	public void mousePressed() {
+		if (isOver) {
+			locked = isOverAndNotPress;
+		}
+	}
+	
+	public void mouseDragged() {
+		if (locked != null) {
+			locked.cur_x = mouseX;
+			locked.cur_y = mouseY;
+		}
 	}
 
+	public void mouseReleased() {
+		haveSame = false;
+		if(locked != null) {
+			// add the node to the big circle
+			if(dist(locked.cur_x, locked.cur_y , width/2, height/2+30) < 250) {
+				locked.inBigCircle = true;
+				// if the mouse is choose one of nodes in the big circle and the mouse is released
+				// in the big circle, don't add the same node to the list(addOrder),so check whether 
+				// the node mouse chose in the big circle have existed in the list(addOrder) or not
+				for(Character i : addOrder) {
+					if(i.getName() == locked.getName()) haveSame = true;
+				}
+				if(haveSame == false) addOrder.add(locked);
+				addToBigCircle();
+			}
+			// remove the node from big circle
+			else {
+				ani = Ani.to(locked, (float) 1.0, "x", locked.getInitX());
+				ani = Ani.to(locked, (float) 1.0, "y", locked.getInitY());
+				locked.inBigCircle = false;
+				addOrder.remove(locked);
+				addToBigCircle();
+			}
+		}
+		// reset the locked because my mouse is not pressed in this moment 
+		locked = null;
+	}
+	
+	// add the node to big circle and set the node's position in to the big circle to 
+	// satisfy that n nodes make regular n-edged shape
+	public void addToBigCircle() {
+		float theta = 0;
+		for(Character i : addOrder) {
+			ani = Ani.to(i, (float) 1, "x", (width/2) + 250 * cos(theta));
+			ani = Ani.to(i, (float) 1, "y", (height/2+30) + 250 * sin(theta));
+			theta += (TWO_PI / addOrder.size());
+		}
+	}
 }
