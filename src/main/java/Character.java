1ecbcc7da3a26b314bdcc07a9f5e32959833347e
lagka <lagkanacl@gmail.com>
2016-05-12 20:10:14 +0800

    ghg

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,7 @@
-package main.java;
+//package main.java;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import processing.core.PApplet;
 
 /**
@@ -9,15 +11,68 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	private String name;
+	private int color, weight, radius;
+	private float cur_x, cur_y, init_x, init_y;
+	//private ArrayList<Character> targets;
+	private HashMap<Character, Integer> relations;
+	private boolean in_circle;
 
-	public Character(MainApplet parent){
+	public Character(MainApplet parent, String name, int color, float x, float y){
 
 		this.parent = parent;
-		
+		this.name = name;
+		this.color = color;
+		this.init_x = x;
+		this.init_y = y;
+		this.cur_x = this.init_x;
+		this.cur_y = this.init_y;
+		this.radius = 40;
+		//this.targets = new ArrayList<Character>();
+		this.relations = new HashMap<Character, Integer>();
 	}
 
 	public void display(){
-
+		this.parent.fill(this.color);
+		this.parent.ellipse(this.cur_x, this.cur_y, this.radius, this.radius);
+		
+		if (in_circle) {
+			for (Character c : relations.keySet()) {
+				this.parent.noFill();
+				this.parent.stroke(0);
+				this.parent.strokeWeight(this.relations.get(c));
+				float a = (600 + (cur_x + c.cur_x) / 2) / 2;
+				float b = (355 + (cur_y + c.cur_y) / 2) / 2;
+				if (c.in_circle)
+					parent.bezier(cur_x, cur_y, a, b, a, b, c.cur_x, c.cur_y);
+			}
+		}
 	}
 	
+	public void return_orign_palce()
+	{
+		this.cur_x = this.init_x;
+		this.cur_y = this.init_y;
+	}
+	
+	public void add_target(Character ch, int weight) {
+		//targets.add(ch);
+		//weights.add(weight);
+		this.relations.put(ch, weight);
+	}
+	
+	public float get_init_x()
+	{
+		return this.init_x;
+	}
+	
+	public float get_init_y()
+	{
+		return this.init_x;
+	}
+	
+	public String get_name()
+	{
+		return this.name;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,15 @@
-package main.java;
 
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import controlP5.ControlP5;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import de.looksgood.ani.Ani;
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -9,24 +18,93 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
-	
-	private final static int width = 1200, height = 650;
-	
+	private String file = "starwars-episode-";
+	private String file2 ="-interactions.json";
+	private int level =1;
+	private JSONObject[] datas;
+	private JSONArray[] nodes;
+	private JSONArray[] links;
+	private final static int width = 1000, height = 670;
+	private ArrayList<Character> characters=new  ArrayList<Character>();
+	private boolean mt;
+	private  ControlP5 button;
+	private Minim minim;
+	private AudioPlayer media;
+	private Ani animation;
 	public void setup() {
 
 		size(width, height);
 		smooth();
 		loadData();
+		//minim=new Minim(this);
+		//media=minim.loadFile(this.getClass().getResource("main/resources/VOIA.mp3").getPath());
+		//media.play();
+		Ani.init(this);
+		button=new ControlP5(this);
+		datas=new JSONObject[8];
+		links=new JSONArray[8];
+		nodes=new JSONArray[8];
+		button.addButton("ButtonAdd").setLabel("ADD ALL Nodes")
+									 .setPosition(600, 40) 	
+									 .setSize(150, 40);
+		button.addButton("ButtonClear").setLabel("Clear")
+		 							   .setPosition(600, 80) 	
+		 							   .setSize(150, 40);
 		
 	}
 
 	public void draw() {
-
+			this.background(255);
+			this.fill(0);
+			this.textSize(40);
+			this.text("Star War "+this.level,300,40);
+			for(Character c:characters){
+				c.display();
+			}
 	}
+	public void ButtonAdd(){
+		for(Character c:characters){
+			//c.setInCircle(true);
+		}
+			AddToCircle();
+	}
+	public void ButtonClear(){
+		
+		for(Character c:characters){
+			//c.setInCircle(false);
+			//animation = Ani.to(c, (float) 0.2, "x", c.get_init_x(),Ani.LINEAR);
+			//animation = Ani.to(c, (float) 0.2, "y", c.get_init_y(),Ani.LINEAR);
+		}
+		
+	}
+	public void mouseReleased(){
+		
+	}
+	public void mouseDragged(){
+		
+	}
+	public void keyPressed(){
+		if(keyCode==KeyEvent.VK_1||(keyCode==KeyEvent.VK_UP&&level==2)||(keyCode==KeyEvent.VK_DOWN&&level==7))level=1;
+		else if(keyCode==KeyEvent.VK_2||(keyCode==KeyEvent.VK_DOWN&&level==1)||(keyCode==KeyEvent.VK_UP&&level==3))level=2;
+		else if(keyCode==KeyEvent.VK_3||(keyCode==KeyEvent.VK_DOWN&&level==2)||(keyCode==KeyEvent.VK_UP&&level==4))level=3;
+		else if(keyCode==KeyEvent.VK_4||(keyCode==KeyEvent.VK_DOWN&&level==3)||(keyCode==KeyEvent.VK_UP&&level==5))level=4;
+		else if(keyCode==KeyEvent.VK_5||(keyCode==KeyEvent.VK_DOWN&&level==4)||(keyCode==KeyEvent.VK_UP&&level==6))level=5;
+		else if(keyCode==KeyEvent.VK_6||(keyCode==KeyEvent.VK_DOWN&&level==5)||(keyCode==KeyEvent.VK_UP&&level==7))level=6;
+		else if(keyCode==KeyEvent.VK_7||(keyCode==KeyEvent.VK_DOWN&&level==6)||(keyCode==KeyEvent.VK_UP&&level==1))level=7;
+	
+	}
+	
 
 	private void loadData(){
-
+		/*for(int i=1;i<=7;i++){
+			datas[i]=loadJSONObject(path + file + Integer.toString(i) + file2);
+			nodes[i]=datas[i].getJSONArray("nodes");
+			links[i]=datas[i].getJSONArray("links");
+		}*/
+	}
+	private void AddToCircle(){
+		
+		
 	}
 
 }
